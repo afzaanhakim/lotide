c383fefd38cae502ce9c3d0b97d906c9a2ec6914
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-05-27 23:28:40 +0000

    implemented findKey using callback and tested

---
+++ findKey.js
@@ -0,0 +1,51 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const findKey = function(name, callback) {
+
+  for (let n in name) {
+    if (callback(name[n])) {
+      return n;
+    }
+  }
+};
+
+
+
+// console.log(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2));
+
+
+
+// assertEqual(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2), "noma");
+
+
+
+// assertEqual(findKey({
+//   "Corvette": { price: 150000 },
+//   "BMW":   { price: 75000 },
+//   "KIA":      { price: 20000 },
+//   "Porsche":   { price: 100000 },
+//   "Lexus":       { price: 70000 },
+//   "Toyota":  { price: 15000 }
+// }, r => r.price === 70000), "Lexus");
