537ec59babf3ea41bcd74862d3057d8a75755bca
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-05-31 18:33:58 +0000

    linting middleTest.js file

--- test/middleTest.js
+++ test/middleTest.js
@@ -21,23 +21,13 @@ describe("#middle", () => {
   });
 
   it("returns [65] for array called grades", () => {
-    let grades = [100, 70,  65 , 90, 85]
+    let grades = [100, 70,  65 , 90, 85];
     assert.deepEqual(middle(grades), [65]);
   });
-
-
 
 });
 
 
 
 
-//test ---->
 
-// middle([1, 2]);
-// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); // => [3, 4]
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]); // => [4]
-// assertArraysEqual(middle([2, 4, 6, 8]), [4,7]); //=> [4, 6] =>should FAIL assertArraysEqual 
-// assertArraysEqual(middle(["hello", "my", "name", "is"]), ["my", "name"]); //=> ["my", "name"]
