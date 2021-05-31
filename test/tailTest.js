f2d71222112cbcd31a5c0938f3ded5fb8efb46a1
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-05-31 18:33:13 +0000

    linting tailtest file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,7 @@
 
 
 const assert = require('chai').assert;
-const tail = require('../tail')
+const tail = require('../tail');
 
 describe("#tail", () => {
   it("returns [2, 3] for [1, 2, 3]", () => {
@@ -9,20 +9,20 @@ describe("#tail", () => {
   });
 
 
-    it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
-      assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 
 
 
   it('returns ["Lighthouse", "Labs"] for ["YoYo", "Lighthouse", "Labs"]', () => {
-    let words = ["YoYo", "Lighthouse", "Labs"]
+    let words = ["YoYo", "Lighthouse", "Labs"];
     assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
   });
 
 
   it('fails to return ["90", "101"] for ["50", "90", "100"]', () => {
-    let result = [50, 90, 100]
+    let result = [50, 90, 100];
     assert.notDeepEqual(tail(result), [90, 101]);
   });
 });
@@ -31,15 +31,3 @@ describe("#tail", () => {
 
 
 
-
-//test1
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result[1], "Labs");
-// assertEqual(result.length, 2);
-// assertEqual(result[0], "Lighthouse");
-
-
-// //test2
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
