2ad3d6ea4767bf49b3646642439fae087f02eebd
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-05-31 17:18:16 +0000

    refactored by requiring assertArraysEqual and mimplementing module export to export middle

--- middle.js
+++ middle.js
@@ -1,27 +1,4 @@
-// const eqArrays = (arr1, arr2) => {
-//   let result = true; //declaring a variable as true to check for future test cases
-//   if (arr1.length !== arr2.length) {//first case to check if the length of arrays are equal
-//     return result = false; //return false if length is not equal
-//   } else {
-//     for (let a = 0; a < arr1.length; a++) {//use a for loop to loop over elements of the first array
-//       if (arr1[a] !== arr2[a]) {//compare if any element of array1  !== element of array2
-//         return result = false; // return as false if any elements are not there in both arrays
-//       }
-//     }
-//   }
-//   return result; // if both the above test conditions fail (checking inequality of array length and checking inequality of array elements; then we return the original result)
-// };
-
-// const assertArraysEqual = (arr1, arr2) => {
-//   if (eqArrays(arr1, arr2) === true) {
-//     console.log(`👍👍👍 Assertion Passed: ${arr1} === ${arr2}`);
-//   } else {
-//     console.log(`😭😭😭 Assertion Failed: ${arr1} !== ${arr2}`);
-//   }
-// };
-
-
-
+const assertArraysEqual = require('./assertArraysEqual')
 const middle = function(array) {
 
   let output = [];
@@ -38,12 +15,5 @@ const middle = function(array) {
 }
 
 
+module.exports = middle;
 
-
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-console.log(middle([1, 2, 3, 4, 5, 6, 7])); // => [4]
-console.log(middle([2, 4, 6, 8])); //=> [4, 6]
-console.log(middle(["hello", "my", "name", "is"])); //=> ["my", "name"]
