3fdda10ac7d1166eab86711832a7cebaae3a859d
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-05-31 12:43:13 +0000

    implemented require logic for tail file

--- tail.js
+++ tail.js
@@ -1,19 +1,16 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require('./assertEqual');
+
 
 let tail = (arr) => {
   let vars = arr.slice(1);
   return vars;
 };
-let arr = ["Hello", "Lighthouse", "Labs"];
-let result = tail(arr);
 
-    
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-assertEqual(arr.length, 3);
+
+module.exports = tail;
+
+
+
+
+
+
