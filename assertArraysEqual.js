c6c7becca68bdf03bdb00aa49e55bf5d83e75a89
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-05-31 16:54:45 +0000

    implemented requirement for eqArrays and created module export for assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,5 @@
-const eqArrays = (arr1, arr2) => {
-  let result = true; //declaring a variable as true to check for future test cases
-  if (arr1.length !== arr2.length) {//first case to check if the length of arrays are equal
-    return result = false; //return false if length is not equal
-  } else {
-    for (let a = 0; a < arr1.length; a++) {//use a for loop to loop over elements of the first array
-      if (arr1[a] !== arr2[a]) {//compare if any element of array1  !== element of array2
-        return result = false; // return as false if any elements are not there in both arrays
-      }
-    }
-  }
-  return result; // if both the above test conditions fail (checking inequality of array length and checking inequality of array elements; then we return the original result)
-};
+
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2) === true) {
@@ -21,6 +10,6 @@ const assertArraysEqual = (arr1, arr2) => {
 };
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);//-----> test cases
-assertArraysEqual(["1", "2", "3"], ["1", "2", "three"]); //-----> test cases
-  
+module.exports = assertArraysEqual;
+
+
