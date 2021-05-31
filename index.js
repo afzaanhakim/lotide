dd66cf13ca724c5e16f45e426ef0a377abb40561
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-05-31 19:14:37 +0000

    referenced all utility functions that the lotide library consists

---
+++ index.js
@@ -0,0 +1,40 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const assertEqual = require('./assertEqual');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const countLetters = require('./countLetters');
+const findKeyByValue = require('./findKeyByValue');
+const map = require('./map');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const letterPositions = require('./letterPositions')
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  without: without,
+  takeUntil: takeUntil,
+  assertEqual: assertEqual,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  countLetters: countLetters,
+  findKeyByValue: findKeyByValue,
+  map: map,
+  assertObjectsEqual: assertObjectsEqual,
+  letterPositions: letterPositions
+
+
+
+
+
+};
