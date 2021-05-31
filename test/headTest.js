85da43d415b3b96372cbe6c078da4651d33923ba
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-05-31 17:41:11 +0000

    implemented test cases using Mocha and Chai framework

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,32 @@
-const assertEqual = require('../assertEqual');
 
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+
+  it("returns 'Lighthouse' for ['Lighthouse', 'Labs]", () => {
+    assert.strictEqual(head(['Lighthouse', 'Labs']), "Lighthouse"); 
+  });
+
+
+  it ("returns 'Hello' for '['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), "Hello")
+
+  });
+
+
+});
 
 
 
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
 
 
